f68262e737c1f7536284838b75504f58e682ed3b
Haydn Vestal <haydnv@gmail.com>
2020-08-18 18:02:11 -0500

    test the API needed to implement a transaction

--- graph.py
+++ graph.py
@@ -12,9 +12,9 @@ class Graph(object):
             schema = Schema(node_key, node_values)
             self.nodes[node_type] = Table(Index(schema))
 
-        self._node_indexs = Table(
-            Index(Schema([("type", str), ("key", tuple)], [("id", int)])))
-        self._node_indexs.add_index("node_index", ("id",))
+        self._node_indices = Table(
+            Index(Schema([("type", str), ("key", tuple)], [("index", int)])))
+        self._node_indices.add_index("node_index", ("index",))
 
         self._edges = {}
         for edge_label, edge_dtype in edge_schema.items():
@@ -65,7 +65,7 @@ class Graph(object):
     def add_node(self, node_type, key, value=tuple()):
         self.nodes[node_type].insert(key + value)
 
-        self._node_indexs.insert([node_type, key, self._max_id])
+        self._node_indices.insert([node_type, key, self._max_id])
         self._max_id += 1
 
         for edges in self._edges.values():
@@ -75,24 +75,6 @@ class Graph(object):
         from_node_index = self._get_node_index(from_node)
         to_node_index = self._get_node_index(to_node)
         self._edges[label][(from_node_index, to_node_index)] = value
-
-    def bft(self, edge_label, node):  # breadth-first traversal
-        edges = self._edges[edge_label]
-
-        node_index = self._get_node_index(node)
-
-        visited = SparseTensor([self._max_id], np.bool)
-
-        adjacent = SparseTensor([self._max_id], np.bool)
-        adjacent[node_index] = True
-
-        while adjacent.any():
-            visited = visited | adjacent
-            adjacent = einsum('ji,j->i', edges, adjacent).copy()
-            adjacent.mask(visited)
-
-            for (i,), _ in adjacent.filled():
-                yield self._get_node(i)
 
     def as_stream(self):
         nodes = {
@@ -107,15 +89,39 @@ class Graph(object):
 
         return (nodes, edges)
 
+    def bft(self, edge_label, node, limit=None):  # breadth-first traversal
+        edges = self._edges[edge_label]
+
+        node_index = self._get_node_index(node)
+
+        visited = SparseTensor([self._max_id], np.bool)
+
+        adjacent = SparseTensor([self._max_id], np.bool)
+        adjacent[node_index] = True
+
+        i = 0
+        while adjacent.any():
+            visited = visited | adjacent
+            adjacent = einsum('ji,j->i', edges, adjacent).copy()
+            adjacent.mask(visited)
+
+            for (i,), _ in adjacent.filled():
+                yield self._get_node(i)
+
+            i += 1
+            if limit and i >= limit:
+                break
+
     def _get_node(self, node_index):
         [node] = list(
-            self._node_indexs.slice({"id": node_index}).select(("type", "key")))
+            self._node_indices.slice({"index": node_index})
+            .select(("type", "key")))
         return node
 
     def _get_node_index(self, node):
         (node_type, node_key) = node
-        [(node_index,)] = list(self._node_indexs.slice({
-            "type": node_type, "key": node_key}).select(["id"]))
+        [(node_index,)] = list(self._node_indices.slice({
+            "type": node_type, "key": node_key}).select(["index"]))
         return node_index
 
     def _stream_edges(self, label):
@@ -181,8 +187,52 @@ def test_equals():
     assert g1 != g2
 
 
+def test_transaction():
+    g = Graph(
+        {"provider": ([("name", str)], [("resolved", bool), ("value", str)])},
+        {"precedes": np.bool, "requires": np.bool})
+
+    providers = g.nodes["provider"]
+
+    providers.add_index("resolved", ("resolved",))
+
+    g.add_node("provider", ("one",), (False, "this",))
+    g.add_node("provider", ("two",), (False, "is",))
+    g.add_node("provider", ("three",), (False, "a",))
+    g.add_node("provider", ("four",), (False, "transaction",))
+
+    g.add_edge("precedes", ("provider", ("one",)), ("provider", ("two",)))
+    g.add_edge("requires", ("provider", ("two",)), ("provider", ("one",)))
+
+    g.add_edge("precedes", ("provider", ("two",)), ("provider", ("three",)))
+    g.add_edge("requires", ("provider", ("three",)), ("provider", ("two",)))
+
+    g.add_edge("precedes", ("provider", ("three",)), ("provider", ("four",)))
+    g.add_edge("requires", ("provider", ("four",)), ("provider", ("three",)))
+
+    done = False
+    while not done:
+        done = True
+        unresolved = g.nodes["provider"].slice({"resolved": False}).select(("name",))
+        for (value_id,) in unresolved:
+            done = False
+            ready = True
+
+            for (_, (name,)) in g.bft("requires", ("provider", (value_id,)), 1):
+                provider = providers.slice({"name": name}).select(("resolved", "value"))
+                [(provider_ready, provider_value)] = list(provider)
+                if not provider_ready:
+                    ready = False
+                    break
+
+            if ready:
+                providers.slice({"name": value_id}).update({"resolved": True})
+
+    pass # no error = test passes
+
 if __name__ == "__main__":
     test_bft()
     test_equals()
+    test_transaction()
     print("PASS")
 
